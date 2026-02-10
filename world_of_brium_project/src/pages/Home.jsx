08f59cf1250ca00db99c66af322ea4d464c358f6
erikqbirch <erikqbirch@gmail.com>
2026-02-09 18:06:06 -0700

    Pushings changes before doing more work

--- world_of_brium_project/src/pages/Home.jsx
+++ world_of_brium_project/src/pages/Home.jsx
@@ -1,16 +1,27 @@
 import './scss/Home.scss';
+import authorImg from '../assets/images/author.jpg';
 
 export default function Home() {
   return (
     <main className="page home">
-      <h2>Home</h2>
-      <p>Welcome to the world of Brium.</p>
-      <article id="home_banner">
+      <section id="home_banner">
+        <article id="home_banner_filter" className='filter'>
+          <h2>Home</h2>
+          <p>Welcome to the world of Brium.</p>
+        </article>
+      </section>
+      <section id="author_intro">
         <h3>Discover Brium</h3>
-        <p>
-          Dive into the enchanting world of Brium, where magic and adventure await at every turn. Explore our collection of books, learn about the lore, and join our community of passionate readers and writers.
-        </p>
-      </article>
+        <figure>
+          <img src={authorImg} alt="author_img" />
+        </figure>
+        <p>Explore the rich lore, characters, and adventures that await you in Brium.</p>
+      </section>
+      <section id="signup">
+        <h3>Stay Updated</h3>
+        <p>Sign up for our newsletter to receive the latest news and updates about Brium.</p>
+        
+      </section>
     </main>
   )
 }
