acfe20678028d3a3a45ae8f87ac31043989b5a2a
erikqbirch <erikqbirch@gmail.com>
2026-02-16 16:59:15 -0700

    Updates

--- world_of_brium_project/src/pages/Home.jsx
+++ world_of_brium_project/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import './scss/Home.scss';
 import authorImg from '../assets/images/author.jpg';
+import sampleBook from '../assets/images/sample_book2.png';
 
 export default function Home() {
   return (
     <main className="page home">
       <section id="home_banner">
         <article id="home_banner_filter" className='filter'>
-          <h2>Home</h2>
-          <p>Welcome to the world of Brium.</p>
+          <div className="text-container">
+            {/* <h2>Home</h2>
+            <p>Welcome to the world of Brium.</p> */}
+          </div>
+          {/* <figure>
+            <img src={sampleBook} alt="sample_book" />
+          </figure> */}
         </article>
       </section>
       <section id="author_intro">
@@ -15,7 +21,7 @@ export default function Home() {
         <figure>
           <img src={authorImg} alt="author_img" />
         </figure>
-        <p>Explore the rich lore, characters, and adventures that await you in Brium.</p>
+        <p>Explore the rich lore, characters, and adventures that await you in Brium. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quasi necessitatibus magnam, enim blanditiis libero. Veniam fuga voluptates nesciunt debitis, iure omnis doloribus obcaecati deserunt mollitia voluptatum id perspiciatis quod saepe corrupti. Totam, error voluptatem! Non quidem veritatis quibusdam, cupiditate modi consequuntur saepe maiores necessitatibus mollitia ipsam numquam odio eaque maxime animi sint quaerat nisi nesciunt aspernatur. Inventore, architecto exercitationem?</p>
       </section>
       <section id="signup">
         <h3>Stay Updated</h3>
