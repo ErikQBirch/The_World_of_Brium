2ab90111e21a49e8741a62cd78da4164d7bbf642
erikqbirch <erikqbirch@gmail.com>
2026-03-09 21:58:56 -0600

    prepping for json work

--- world_of_brium_project/src/components/Footer.jsx
+++ world_of_brium_project/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 // import logoImg from '../../public/images/placeholder.jpg';
 import './scss/Footer.scss'
