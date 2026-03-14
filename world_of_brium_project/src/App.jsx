7fd197ab9c3cd08d7feb60069a001fe4da23964b
erikqbirch <erikqbirch@gmail.com>
2026-03-14 15:24:47 -0600

    Attempt12

--- world_of_brium_project/src/App.jsx
+++ world_of_brium_project/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -12,7 +12,7 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <BrowserRouter basename='/The_World_of_Brium'>
+    <HashRouter>
       <NavBar />
       <div className="container">
         <Routes>
@@ -28,7 +28,7 @@ function App() {
       <section>
         <Footer />
       </section>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
